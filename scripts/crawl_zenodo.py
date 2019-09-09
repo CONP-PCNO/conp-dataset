59aac034e33543df6b6a8cc461e2a9907a0dad72
joeyzhou98 <joeyzhou7002@gmail.com>
2019-09-08 22:24:57 -0400

    [DATALAD] Recorded existing changes

--- scripts/crawl_zenodo.py
+++ scripts/crawl_zenodo.py
@@ -1,9 +1,17 @@
 import requests
 import os
 import json
+import sys
+from zipfile import ZipFile
+import datalad.api as api
+from re import sub
 
 
 def crawl():
+
+    # Get github username and password
+    token = get_token()
+
     zenodo_dois = get_zenodo_dois()
     conp_dois = get_conp_dois()
 
@@ -17,12 +25,18 @@ def crawl():
         if index is not None:
             # If the conp dataset version isn't the lastest, update
             if dataset["latest_version"] != conp_dois[index]["version"]:
-                # Update
+                pass
         else:
-            # Update
+            create_new_dataset(dataset, token)
 
 
-    print(conp_dois)
+def get_token():
+    if len(sys.argv) != 2:
+        raise Exception("Need to pass Github access token")
+
+    token = sys.argv[1]
+
+    return token
 
 
 def get_conp_dois():
@@ -72,16 +86,25 @@ def get_zenodo_dois():
     r = requests.get("https://zenodo.org/api/records/?"
                      # "type=dataset&"
                      # "q=keywords:\"canadian-open-neuroscience-platform\"&"
-                     "q=keywords:\"analysis\"&"
-                     "size=20").json()["hits"]["hits"]
+                     # "q=keywords:\"analysis\"&"
+                     "q=3363060"
+                     "size=1").json()["hits"]["hits"]
     for dataset in r:
         concept_doi = dataset["conceptrecid"]
         if len(dataset["metadata"]["relations"]["version"]) != 1:
             raise Exception("Unexpected multiple versions")
         latest_version_doi = dataset["metadata"]["relations"]["version"][0]["last_child"]["pid_value"]
+        zip_files = []
+        for bucket in dataset["files"]:
+            if bucket["type"] == "zip":
+                zip_files.append(bucket["links"]["self"])
+        if len(zip_files) < 1:
+            print("Zenodo dataset " + dataset["title"] + " does not contain a zip file")
         zenodo_dois.append({
             "concept_doi": concept_doi,
-            "latest_version": latest_version_doi
+            "latest_version": latest_version_doi,
+            "title": clean(dataset["metadata"]["title"]),
+            "files": zip_files
         })
 
     return zenodo_dois
@@ -94,6 +117,12 @@ def verify_duplicates(zenodo_dois, conp_dois):
     version_dois = list(map(lambda x: x["latest_version"], zenodo_dois))
     if len(version_dois) != len(set(version_dois)):
         raise Exception("Version DOI duplicates exists in zenodo list")
+    titles = list(map(lambda x: x["title"], zenodo_dois))
+    if len(titles) != len(set(titles)):
+        raise Exception("Title duplicates exists in zenodo list")
+    file_link = list(map(lambda x: x["file"], zenodo_dois))
+    if len(file_link) != len(set(file_link)):
+        raise Exception("File link duplicates exists in zenodo list")
     concept_dois = list(map(lambda x: x["concept_doi"], conp_dois))
     if len(concept_dois) != len(set(concept_dois)):
         raise Exception("Concept DOI duplicates exists in conp list")
@@ -103,6 +132,28 @@ def verify_duplicates(zenodo_dois, conp_dois):
     directories = list(map(lambda x: x["directory"], conp_dois))
     if len(directories) != len(set(directories)):
         raise Exception("Directory duplicates exists in conp list")
+
+
+clean = lambda x: sub('\W|^(?=\d)','_', x)
+
+
+def create_new_dataset(dataset, token):
+    dir = os.path.join("projects", dataset["title"])
+    api.create(dir)
+    api.create_sibling_github("conp-dataset-" + dataset["title"],
+                              dataset=dir,
+                              recursive=True,
+                              github_login=token,
+                              github_passwd=token)
+    for file_url in dataset["files"]:
+        r = requests.get(file_url)
+        if r.ok:
+            with open("temp.zip", "wb") as f:
+                f.write(r.content)
+            with ZipFile("temp.zip", "r") as f:
+                f.extractall(dir)
+        else:
+            raise Exception("Failed to download zip file: " + file_url)
 
 
 if __name__ == "__main__":
