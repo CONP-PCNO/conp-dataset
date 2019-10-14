3371f5ab52c153442295c49f47418f60eed9827a
joeyzhou98 <joeyzhou7002@gmail.com>
2019-10-13 23:25:24 -0400

    allow users to create tests for all submodules if diff.txt does not exist

--- tests/create_tests.py
+++ tests/create_tests.py
@@ -1,24 +1,29 @@
 from string import Template
 from git import Repo
+from os.path import isfile
 
 
 submodules = list(map(lambda x: x.path, Repo(".").submodules))
-
-with open("tests/diff.txt", "r") as f:
-    changes = f.readlines()
-
 changed_submodules = set()
-for line in changes:
-    for submodule in submodules:
-        if submodule in line:
-            changed_submodules.add(submodule)
 
-if len(changed_submodules) > 0:
-    print("Detected changes in the following submodules:")
-    for submodule in changed_submodules:
-        print(submodule)
+if isfile("tests/diff.txt"):
+    with open("tests/diff.txt", "r") as f:
+        changes = f.readlines()
+
+    for line in changes:
+        for submodule in submodules:
+            if submodule in line:
+                changed_submodules.add(submodule)
+
+    if len(changed_submodules) > 0:
+        print("Detected changes in the following submodules, creating tests for them:")
+        for submodule in changed_submodules:
+            print(submodule)
+    else:
+        print("No changes in submodules")
 else:
-    print("No changes in submodules")
+    print("No diff.txt file detected, creating tests for every submodule")
+    changed_submodules = set(submodules)
 
 
 template = Template("""from functions import examine
