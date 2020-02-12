87d4388504e50204bd249a21fe833f9567ab45fd
joeyzhou98 <joeyzhou7002@gmail.com>
2020-02-12 17:04:09 -0500

    annex addurl file name is now shorter

--- scripts/crawl_zenodo.py
+++ scripts/crawl_zenodo.py
@@ -562,9 +562,9 @@ def download_file(bucket, d, dataset_dir):
             d.download_url(link, archive=True if bucket["type"] == "zip" else False)
         else:
             try:  # Try to addurl twice as rarely it might not work on the first try
-                annex("addurl", link, "--fast")
+                annex("addurl", link, "--fast", "--file", link.split("/")[-1])
             except GitCommandError:
-                annex("addurl", link, "--fast")
+                annex("addurl", link, "--fast", "--file", link.split("/")[-1])
     else:  # Have to remove token from annex URL
         if bucket["type"] == "zip":
             file_path = d.download_url(link)[0]["path"]
