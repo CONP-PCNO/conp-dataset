094cafe5b8067554ccb54b6a25849cd8e590689d
mathdugre <math.dugre@gmail.com>
2020-02-18 12:05:30 -0500

    [REFACTORED] function signature for test template

--- tests/create_tests.py
+++ tests/create_tests.py
@@ -1,19 +1,54 @@
+import re
 from string import Template
+
 from git import Repo
+
+
+def project_name2env(project_name: str) -> str:
+    """Convert the project name to a valid ENV var name.
+
+    The ENV name for the project must match the regex `[a-zA-Z_]+[a-zA-Z0-9_]*`.
+
+    Parameters
+    ----------
+    project_name: str
+        Name of the project.
+
+    Return
+    ------
+    project_env: str
+        A valid ENV name for the project.
+    """
+    project_name = project_name.replace("-", "_")
+    project_env = re.sub("[_]+", "_", project_name)  # Remove consecutive `_`
+    project_env = re.sub("[^a-zA-Z0-9_]", "", project_env)
+
+    # Env var cannot start with number
+    if re.compile("[0-9]").match(project_env[0]):
+        project_env = "_" + project_env
+
+    return project_env.upper()
 
 
 submodules = list(map(lambda x: x.path, Repo(".").submodules))
 
-template = Template("""from functions import examine
+template = Template(
+    """from functions import examine
 
 
 def test_$clean_title():
-    assert examine('$path') == 'All good'
-""")
+    assert examine('$path', '$project') == 'All good'
+"""
+)
 
 for dataset in submodules:
     if dataset.split("/")[0] == "projects" or dataset.split("/")[0] == "investigators":
         with open("tests/test_" + dataset.replace("/", "_") + ".py", "w") as f:
 
-            f.write(template.substitute(path=dataset,
-                                        clean_title=dataset.replace("/", "_").replace("-", "_")))
+            f.write(
+                template.substitute(
+                    path=dataset,
+                    project=project_name2env(dataset.split("/")[-1]),
+                    clean_title=dataset.replace("/", "_").replace("-", "_"),
+                )
+            )
--- tests/functions.py
+++ tests/functions.py
@@ -1,4 +1,5 @@
 from contextlib import contextmanager
+import os
 from os import listdir, walk
 from os.path import isdir, exists, join, abspath, basename, dirname
 from random import random
@@ -29,12 +30,13 @@ def raise_timeout(signum, frame):
 
 
 def generate_datalad_provider(loris_api):
-    
-    # Regex for provider
-    re_loris_api = loris_api.replcae(".", "\.")
 
-    with open("~/.config/datalad/providers/loris.cfg", 'w') as fout:
-        fout.write(f"""[provider:loris]                                                                    
+    # Regex for provider
+    re_loris_api = loris_api.replace(".", "\.")
+
+    with open("~/.config/datalad/providers/loris.cfg", "w") as fout:
+        fout.write(
+            f"""[provider:loris]                                                                    
 url_re = {re_loris_api}/*                             
 authentication_type = loris-token                                                           
 credential = loris                                                                  
@@ -42,7 +44,8 @@ credential = loris
 [credential:loris]                                                                  
 url = {loris_api}/login                                           
 type = loris-token  
-""")
+"""
+        )
 
 
 def recurse(directory, odds):
@@ -73,27 +76,37 @@ def recurse(directory, odds):
         # If the file is a broken symlink and with odd chance
         elif not exists(full_path) and random() < odds:
             # Timeouts the download or a hanging authentification
-            with timeout(3): 
-                msg = api.get(path=full_path, on_failure="ignore", return_type="item-or-list")
-                
+            with timeout(3):
+                msg = api.get(
+                    path=full_path, on_failure="ignore", return_type="item-or-list"
+                )
+
                 # Check for authentication
-                if isinstance(msg, dict) and msg["status"] == "error" and "unable to access" not in msg["message"].lower():
-                    return "Cannot download file and didn't hit authentication request for file: " + full_path
+                if (
+                    isinstance(msg, dict)
+                    and msg["status"] == "error"
+                    and "unable to access" not in msg["message"].lower()
+                ):
+                    return (
+                        "Cannot download file and didn't hit authentication request for file: "
+                        + full_path
+                    )
 
     return "All good"
 
 
-def examine(dataset, *, username=None, password=None, loris_api=None):
+def examine(dataset, *, project=None):
 
     # If authentication is required, add credentials to the keyring
     # and create a provider config file.
     # Note: Assume a loris-token authntification.
-    if username and password and loris_api:
-        keyring.set_password("datalad-loris", "user", username)
-        keyring.set_password("datalad-loris", "password", password)
-        generate_datalad_provider(loris_api)
-    elif username or password or loris_api:
-        raise Exception("Insuficient information for authetication.")
+    if project:
+
+        keyring.set_password("datalad-loris", "user", os.environ[project + "_USERNAME"])
+        keyring.set_password(
+            "datalad-loris", "password", os.environ[project + "_PASSWORD"]
+        )
+        generate_datalad_provider(os.environ[project + "_LORIS_API"])
 
     # Check if dats.json and README.md are present in root of dataset
     file_names = [file_name for file_name in listdir(dataset)]
@@ -106,12 +119,17 @@ def examine(dataset, *, username=None, password=None, loris_api=None):
     # Number of files to test in each dataset
     # with 100 files, the test is not completing before Travis timeout (about 10~12 minutes)
     num_files = 4
-    
+
     # Count the number of testable files while ignoring files in directories starting with "."
-    count = sum([len(files) if basename(dirname(r))[0] != "." else 0 for r, d, files in walk(dataset)])
+    count = sum(
+        [
+            len(files) if basename(dirname(r))[0] != "." else 0
+            for r, d, files in walk(dataset)
+        ]
+    )
 
     # Calculate the odds to test a file
-    odds = num_files/count
+    odds = num_files / count
 
     # Start to test dataset
     return recurse(abspath(dataset), odds)
