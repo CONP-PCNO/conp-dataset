f6ce35dbe0563d673d8a77d0b38c64f7a2b50749
mathdugre <math.dugre@gmail.com>
2020-03-09 14:30:56 -0400

    [FORMAT] apply black formatting

--- tests/create_tests.py
+++ tests/create_tests.py
@@ -4,16 +4,22 @@ from git import Repo
 
 submodules = list(map(lambda x: x.path, Repo(".").submodules))
 
-template = Template("""from functions import examine
+template = Template(
+    """from functions import examine
 
 
 def test_$clean_title():
     assert examine('$path') == 'All good'
-""")
+"""
+)
 
 for dataset in submodules:
     if dataset.split("/")[0] == "projects" or dataset.split("/")[0] == "investigators":
         with open("tests/test_" + dataset.replace("/", "_") + ".py", "w") as f:
 
-            f.write(template.substitute(path=dataset,
-                                        clean_title=dataset.replace("/", "_").replace("-", "_")))
+            f.write(
+                template.substitute(
+                    path=dataset,
+                    clean_title=dataset.replace("/", "_").replace("-", "_"),
+                )
+            )
