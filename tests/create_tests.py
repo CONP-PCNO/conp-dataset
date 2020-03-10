7a3b860b5b93eb710c0996a8f739774360e43ba1
mathdugre <math.dugre@gmail.com>
2020-03-10 17:04:06 -0400

    [CREATE] implementation for skipping non-required tests.

--- tests/create_tests.py
+++ tests/create_tests.py
@@ -1,8 +1,57 @@
+import os
 from string import Template
+from typing import List
 from git import Repo
+import requests
 
 
-submodules = list(map(lambda x: x.path, Repo(".").submodules))
+def minimal_tests(datasets: List[str], pr_files: List[str]):
+    """Return the minimal test set for a pull request.
+
+    Parameters
+    ----------
+    datasets : List[str]
+        List of datasets in the repository.
+    pr_files : List[str]
+        List of files modified by the pull request.
+    
+    Returns
+    -------
+    [type]
+        [description]
+    """
+    WHITELIST_EXACT: List[str] = [
+        ".datalad",
+        ".github",
+        "docs",
+        "metadata",
+        ".gitattributes",
+    ]
+    WHITELIST: List[str] = [
+        ".gitignore",
+        "README.md",
+        "LICENSE",
+    ]
+
+    # No need to do tests when no modification are brought.
+    if len(pr_files) == 0:
+        return []
+
+    modified_datasets: List[str] = []
+    for pr_filename in pr_files:
+        for dataset in datasets:
+            if dataset in pr_filename:
+                modified_datasets.append(dataset)
+                break
+        else:
+            if pr_filename.split("/")[0] in WHITELIST_EXACT:
+                continue
+            if any([filename in pr_filename for filename in WHITELIST]):
+                continue
+            return datasets
+
+    return modified_datasets
+
 
 template = Template(
     """from functions import examine
@@ -13,7 +62,20 @@ def test_$clean_title():
 """
 )
 
-for dataset in submodules:
+datasets: List[str] = list(map(lambda x: x.path, Repo(".").submodules))
+
+# Detect if we should skip tests for a dataset.
+# This prevent all dataset to be tested on every PR build.
+pull_number = os.getenv("TRAVIS_PULL_REQUEST")  # False if not a PR
+if pull_number:
+    response = requests.get(
+        f"https://api.github.com/repos/CONP-PCNO/conp-dataset/pulls/{pull_number}/files"
+    )
+    pr_files: List[str] = [data["filename"] for data in response.json()]
+
+    datasets = minimal_tests(datasets, pr_files)
+
+for dataset in datasets:
     if dataset.split("/")[0] == "projects" or dataset.split("/")[0] == "investigators":
         with open("tests/test_" + dataset.replace("/", "_") + ".py", "w") as f:
 
