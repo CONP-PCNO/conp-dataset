41abe49f6cfed44bc088c49785676df5ca691c56
joeyzhou98 <joeyzhou7002@gmail.com>
2020-03-12 19:27:09 -0400

    fix shell script

--- scripts/crawl_zenodo.sh
+++ scripts/crawl_zenodo.sh
@@ -8,8 +8,7 @@ mkdir -p ${BASEDIR}/log
 DATE=$(date)
 LOGFILE=$(mktemp ${BASEDIR}/log/crawler-XXXXX.log)
 TOUCHFILE=${BASEDIR}/.crawling
-# Add user tokens here (separated by space), or edit file $HOME/.token
-TOKEN_LIST="w4M00bgKOLzWDCHdFGeXc2wzaE9ftmedZAcneqTQEO9qQK4G3A7ez7CfOS7Y"
+# Edit file $HOME/.conp_crawler_config.json to add tokens
 
 echo  "**** STARTING ZENODO CRAWL at ${DATE}, LOGGING IN ${LOGFILE} ****" &>>$HOME/crawl_zenodo.log
 test -f ${TOUCHFILE} && (echo "Another crawling process is still running (${TOUCHFILE} exists), exiting!" &>>${LOGFILE}; exit 1 )
@@ -19,4 +18,4 @@ touch ${TOUCHFILE}
 
 (cd ${BASEDIR} && git pull --no-edit main master)
 
-python3 ./scripts/crawl_zenodo.py --verbose -z ${TOKEN_LIST} &>>${LOGFILE}
+python3 ./scripts/crawl_zenodo.py --verbose &>>${LOGFILE}
