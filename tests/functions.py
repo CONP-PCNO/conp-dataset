ba364315da2b5f48c1614a7f9de7a27a72a00699
mathdugre <mathdugre@pm.me>
2020-04-23 13:21:00 -0400

    [REFACTOR] Moved the timeout trigger inside the timeout function.

--- tests/functions.py
+++ tests/functions.py
@@ -19,6 +19,9 @@ from scripts.dats_validator.validator import validate_json
 
 @contextmanager
 def timeout(time):
+    def raise_timeout(signum, frame):
+        raise TimeoutError
+
     # Register a function to raise a TimeoutError on the signal.
     signal.signal(signal.SIGALRM, raise_timeout)
     # Schedule the signal to be sent after ``time``.
@@ -32,10 +35,6 @@ def timeout(time):
         # Unregister the signal so it won't be triggered
         # if the timeout is not reached.
         signal.signal(signal.SIGALRM, signal.SIG_IGN)
-
-
-def raise_timeout(signum, frame):
-    raise TimeoutError
 
 
 def project_name2env(project_name: str) -> str:
