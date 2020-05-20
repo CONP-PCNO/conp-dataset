4f25aeb595aed05a671306d028309b1b4eebf940
joeyzhou98 <joeyzhou7002@gmail.com>
2020-05-20 06:31:50 -0400

    handle individual dataset repo not having token in origin url

--- scripts/Crawlers/BaseCrawler.py
+++ scripts/Crawlers/BaseCrawler.py
@@ -231,6 +231,12 @@ class BaseCrawler:
             branch_name = "conp-bot/" + clean_title
             dataset_dir = os.path.join("projects", clean_title)
             d = self.datalad.Dataset(dataset_dir)
+            # Add github token to individual dataset remote urls
+            origin = git.Repo(dataset_dir).remote("origin")
+            origin_url = next(origin.urls)
+            if "@" not in origin_url:
+                origin.set_url(origin_url.replace("https://", "https://" + self.github_token + "@"))
+
             if branch_name not in self.repo.remotes.origin.refs:  # New dataset
                 self.repo.git.checkout("-b", branch_name)
                 repo_title = ("conp-dataset-" + dataset_description["title"])[0:100]
