fab03c89494c530c4719c2931790a46f048c1e1b
Tristan Glatard <tristan.glatard@concordia.ca>
2020-06-22 22:40:27 -0400

    Reverting last commit

--- tests/functions.py
+++ tests/functions.py
@@ -227,6 +227,11 @@ def authenticate(dataset):
 def get_filenames(dataset):
     annex_list: str = git.Repo(dataset).git.annex("list")
     filenames: List[str] = re.split(r"\n[_X]+\s", annex_list)[1:]
+    filenames += [
+            os.path.join(dataset, filename)
+            for filename in re.split(r"\n[_X]+\s", annex_list)[1:]
+    ]
+    return filenames
 
 
 def download_files(dataset, filenames, time_limit=120):
