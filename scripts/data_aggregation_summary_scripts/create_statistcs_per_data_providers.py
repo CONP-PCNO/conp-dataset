a34cce1c02d0c165d2e54cb48a2d82a0f88a65cc
Cécile <cecile.madjar@mcin.ca>
2020-09-24 19:32:31 -0400

    first iteration, need to fix authentication and size that are wrong

---
+++ scripts/data_aggregation_summary_scripts/create_statistcs_per_data_providers.py
@@ -0,0 +1,176 @@
+import getopt
+import sys
+import os
+import lib.Utility as Utility
+
+
+def main(argv):
+
+    conp_dataset_dir = parse_input(argv)
+
+    dataset_descriptor_list = Utility.read_conp_dataset_dir(conp_dataset_dir)
+
+    datasets_summary_dict = {}
+    i = 0
+    for dataset in dataset_descriptor_list:
+        datasets_summary_dict[i] = parse_dats_information(dataset)
+        i += 1
+
+    csv_content = [
+        [
+            'Data Provider',
+            'Number Of Datasets',
+            'Number Of Datasets Requiring Authentication',
+            'Total Number Of Files',
+            'Total Size (GB)',
+            'CONP Status',
+            'Keywords Describing The Data'
+        ]
+    ]
+    for data_provider in ['braincode', 'frdr', 'loris', 'osf', 'zenodo']:
+        summary_list = get_stats_for_data_provider(datasets_summary_dict, data_provider)
+        csv_content.append(summary_list)
+
+    Utility.write_csv_file('summary_statistics_per_data_providers', csv_content)
+
+
+def parse_input(argv):
+
+    conp_dataset_dir = None
+
+    description = '\nThis tool facilitates the creation of statistics per data providers for reporting purposes.' \
+                  ' It will read DATS files and print out a summary per data providers based on the following list' \
+                  'of DATS fields present in the DATS. json of every dataset present in the conp-dataset/projects' \
+                  'directory.\n Queried fields: <distribution->access->landingPage>; <distributions->access->authorizations>; ' \
+                  '<distributions->size>; <extraProperties->files>; <keywords>\n'
+    usage = (
+        '\n'
+        'usage  : python ' + __file__ + ' -d <conp-dataset directory path>\n\n'
+        'options: \n'
+            '\t-d: path to the conp-dataset directory to parse\n'
+    )
+
+    try:
+        opts, args = getopt.getopt(argv, "hd:")
+    except getopt.GetoptError:
+        sys.exit()
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print(description + usage)
+            sys.exit()
+        elif opt == '-d':
+            conp_dataset_dir = arg
+
+    if not conp_dataset_dir:
+        print('a path to the conp-dataset needs to be given as an argument to the script by using the option `-d`')
+        print(description + usage)
+        sys.exit()
+
+    if not os.path.exists(conp_dataset_dir + '/projects'):
+        print(conp_dataset_dir + 'does not appear to be a valid path and does not include a `projects` directory')
+        print(description + usage)
+        sys.exit()
+
+    return conp_dataset_dir
+
+
+def parse_dats_information(dats_dict):
+
+    extra_properties = dats_dict['extraProperties']
+    keywords = dats_dict['keywords']
+
+    values_dict = {
+        'extraProperties': {},
+        'keywords': []
+    }
+    for extra_property in extra_properties:
+        values_dict[extra_property['category']] = extra_property['values'][0]['value']
+    for keyword in keywords:
+        values_dict['keywords'].append(keyword['value'])
+
+    authorization = 'unknown'
+    if 'authorizations' in dats_dict['distributions'][0]['access']:
+        authorization = dats_dict['distributions'][0]['access']['authorizations'][0]['value']
+
+    return {
+        'title'          : dats_dict['title'],
+        'data_provider'  : dats_dict['distributions'][0]['access']['landingPage'],
+        'authorization'  : authorization,
+        'dataset_size'   : dats_dict['distributions'][0]['size'],
+        'size_unit'      : dats_dict['distributions'][0]['unit']['value'],
+        'number_of_files': values_dict['files']        if 'files'       in values_dict else '',
+        'conp_status'    : values_dict['CONP_status']  if 'CONP_status' in values_dict else '',
+        'keywords'       : values_dict['keywords']     if 'keywords'    in values_dict else '',
+    }
+
+
+def get_stats_for_data_provider(dataset_summary_dict, data_provider):
+
+    dataset_number  = 0
+    requires_login  = 0
+    total_size      = 0
+    total_files     = 0
+    total_conp_data = 0
+    total_canadian  = 0
+    total_external  = 0
+    keywords_list   = []
+
+    for index in dataset_summary_dict:
+
+        dataset_dict = dataset_summary_dict[index]
+        if data_provider not in dataset_dict['data_provider']:
+            continue
+
+        dataset_number += 1
+        if isinstance(dataset_dict['number_of_files'], str):
+            total_files += int(dataset_dict['number_of_files'].replace(',', ''))
+        else:
+            total_files += dataset_dict['number_of_files']
+
+        print(dataset_dict['authorization'])
+
+        if dataset_dict['authorization'] in ['private', 'restricted']:
+            requires_login += 1
+
+        if dataset_dict['size_unit'] == 'KB':
+            total_size += dataset_dict['dataset_size'] * pow(1024, 2)
+        elif dataset_dict['size_unit'] == 'MB':
+            total_size += dataset_dict['dataset_size'] * 1024
+        elif dataset_dict['size_unit'] == 'GB':
+            total_size += dataset_dict['dataset_size']
+        elif dataset_dict['size_unit'] == 'TB':
+            total_size += dataset_dict['dataset_size'] / 1024
+        elif dataset_dict['size_unit'] == 'PB':
+            total_size += dataset_dict['dataset_size'] / pow(1024, 2)
+
+        if dataset_dict['conp_status'] == 'CONP':
+            total_conp_data += 1
+        elif dataset_dict['conp_status'] == 'Canadian':
+            total_canadian += 1
+        elif dataset_dict['conp_status'] == 'external':
+            total_external += 1
+
+        for keyword in dataset_dict['keywords']:
+            if keyword not in keywords_list:
+                if keyword == 'canadian-open-neuroscience-platform':
+                    continue
+                keywords_list.append(keyword)
+
+    conp_status_summary = 'CONP (' + str(total_conp_data) + '); ' \
+                          'Canadian (' + str(total_canadian) + '); ' \
+                          'External (' + str(total_external) + ')'
+
+    return [
+        data_provider,
+        str(dataset_number),
+        str(requires_login),
+        str(total_files),
+        str(total_size),
+        conp_status_summary,
+        ', '.join(keywords_list)
+    ]
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
