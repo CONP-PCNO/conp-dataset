e62fcf99ef3869e7cdd0bd58be34f04341d36d10
Cécile <cecile.madjar@mcin.ca>
2021-01-18 18:34:12 -0500

    fix template impression of errors returned by validate_non_schema_required

--- tests/template.py
+++ tests/template.py
@@ -63,7 +63,7 @@ class Template(object):
             if not is_valid:
                 summary_error_message = f"Dataset {dataset} contains DATS.json that has errors " \
                                         f"in required extra properties or formats. List of errors:\n",
-                for i, error_message in errors:
+                for i, error_message in enumerate(errors, 1):
                     summary_error_message += f"- {i} - {error_message}\n"
                 pytest.fail(
                     summary_error_message,
