b8860e7383b88d5c8d3bc9762fd189628d28d6ac
Cécile <cecile.madjar@mcin.ca>
2021-03-04 09:50:21 -0500

    add privacy checks to DATS validator

--- scripts/dats_validator/validator.py
+++ scripts/dats_validator/validator.py
@@ -175,8 +175,33 @@ def validate_date_types(dataset):
         return True, errors_list
 
 
+def validate_privacy(dataset):
+    """
+    Checks if the values in the privacy field of the JSON object is one of:
+    - open
+    - registered
+    - controlled
+    - private
+    '"""
+
+    errors_list = []
+    valid_privacy_values = ['open', 'registered', 'controlled', 'private']
+
+    if 'privacy' in dataset.keys():
+        if dataset['privacy'] not in valid_privacy_values:
+            error_message = f"Validation error in {dataset['title']}: privacy " \
+                            f"- '{dataset['privacy']}' is not allowed. Allowed " \
+                            f"value should be one of {valid_privacy_values}. "
+            errors_list.append(error_message)
+
+    if errors_list:
+        return False, errors_list
+    else:
+        return True, errors_list
+
+
 def validate_recursively(obj, errors):
-    """ Checks all datasets recursively for required extraProperties. """
+    """ Checks all datasets recursively for non-schema checks. """
 
     val, errors_list = validate_extra_properties(obj)
     errors.extend(errors_list)
@@ -184,6 +209,9 @@ def validate_recursively(obj, errors):
     errors.extend(errors_list)
     val, errors_list = validate_date_types(obj)
     errors.extend(errors_list)
+    val, errors_list = validate_privacy(obj)
+    errors.extend(errors_list)
+
     if "hasPart" in obj:
         for each in obj["hasPart"]:
             validate_recursively(each, errors)
