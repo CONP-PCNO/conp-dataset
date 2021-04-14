7d7737ec77d4b273a7633748076675b9ed52e079
mathdugre <mathdugre@pm.me>
2021-04-14 14:02:52 -0400

    [REFACTOR] fix pre-commit hooks issues.

--- scripts/Crawlers/OSFCrawler.py
+++ scripts/Crawlers/OSFCrawler.py
@@ -328,15 +328,15 @@ class OSFCrawler(BaseCrawler):
                     {
                         "date": date_created.strftime("%Y-%m-%d %H:%M:%S"),
                         "type": {
-                            "value": "date created"
-                        }
+                            "value": "date created",
+                        },
                     },
                     {
                         "date": date_modified.strftime("%Y-%m-%d %H:%M:%S"),
                         "type": {
-                            "value": "date modified"
-                        }
-                    }
+                            "value": "date modified",
+                        },
+                    },
                 ],
                 "keywords": keywords,
                 "distributions": [
