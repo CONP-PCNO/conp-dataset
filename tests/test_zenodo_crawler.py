e06badb39b7323188ce2635f2327026a8fbcd2bb
joeyzhou98 <joeyzhou7002@gmail.com>
2021-07-05 11:32:49 -0400

    fix tests

--- tests/test_zenodo_crawler.py
+++ tests/test_zenodo_crawler.py
@@ -63,6 +63,7 @@ def mock_get_test_dataset_dir():
 
 
 class TestZenodoCrawler(TestCase):
+    @mock.patch("scripts.Crawlers.BaseCrawler.BaseCrawler._check_dats_present", return_value=None)
     @mock.patch("scripts.Crawlers.ZenodoCrawler.ZenodoCrawler._push_and_pull_request")
     @mock.patch("scripts.Crawlers.ZenodoCrawler.ZenodoCrawler._create_readme")
     @mock.patch("scripts.Crawlers.ZenodoCrawler.ZenodoCrawler.get_readme_content")
@@ -91,6 +92,7 @@ class TestZenodoCrawler(TestCase):
         mock_get_readme,
         mock_create_readme,
         mock_create_pr,
+        mock_check_dats_present,
     ):
         try:
             ZenodoCrawler(
