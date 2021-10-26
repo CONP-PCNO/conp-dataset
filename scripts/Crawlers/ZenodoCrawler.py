af29d4d532b5f6dbf9c7e1bc7be844fea319b3fe
cmadjar <cecile.madjar@gmail.com>
2021-10-26 15:45:28 -0400

    fix precommit

--- scripts/Crawlers/ZenodoCrawler.py
+++ scripts/Crawlers/ZenodoCrawler.py
@@ -141,12 +141,12 @@ class ZenodoCrawler(BaseCrawler):
 
             # Retrieve subject and clean subject to insert in isAbout of DATS file
             is_about = []
-            if 'subjects' in metadata.keys():
+            if "subjects" in metadata.keys():
                 for subject in metadata["subjects"]:
                     is_about.append(
                         {
                             "identifier": {"identifier": subject["identifier"]},
-                            "name": subject["term"]
+                            "name": subject["term"],
                         }
                     )
 
